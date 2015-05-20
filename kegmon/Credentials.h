daa78c5ab3c19bd5669a52b7e4e707725612cfe0
Niel McLaren <dan@danielmclaren.com>
2015-05-19 22:50:29 -0700

    No wifi, use OLED display instead.

--- kegmon/Credentials.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CREDENTIALS_H__
-#define __CREDENTIALS_H__
-
-char ssid[] = "<ssid>";
-char passphrase[] = "<passphrase>";
-String xivelyApiKey = "<apikey>";
-
-#endif
--- kegmon/kegmon.ino
+++ kegmon/kegmon.ino
@@ -1,381 +1,56 @@
-#include "SPI.h"
-#include "WiFly.h"
-#include "Credentials.h"
+#include <Adafruit_CharacterOLED.h>
 
-WiFlyClient client("api.xively.com" , 80);
+/**
+ * The circuit:
+ * LCD RS pin to digital pin 6
+ * LCD R/W pin to digital pin 7
+ * LCD Enable pin to digital pin 8
+ * LCD D4 pin to digital pin 9
+ * LCD D5 pin to digital pin 10
+ * LCD D6 pin to digital pin 11
+ * LCD D7 pin to digital pin 12
+ */
+
+Adafruit_CharacterOLED lcd(OLED_V2, 6, 7, 8, 9, 10, 11, 12);
 
 unsigned long startTime;
-
-#define RESPONSE_BUFFER_SIZE 128
-char responseBuffer[RESPONSE_BUFFER_SIZE];
-
-char requestHttpCommand[] = "PUT /v2/feeds/734040001.json HTTP/1.1";
-char requestApiKey[] = "X-ApiKey: ";
-char requestUserAgent[] = "User-Agent: Arduino/1.0";
-char requestHost[] = "Host: api.xively.com";
-char requestContentType[] = "Content-Type: text/json";
-char requestContentLength[] = "Content-Length: 80";
-char requestConnection[] = "Connection: close";
-char requestDataPrefix[] = "{\"version\":\"1.0.0\",\"datastreams\": [{\"id\":\"forceSensor\",\"current_value\":\"";
-char requestDataSuffix[] = "\"}]}";
-char http200[] = "HTTP/1.1 200 OK";
-
-#define MAX_RETRIES 3
-int retries;
-
-int state;
-
 unsigned long readingSum;
 int numReadings;
 
-#define STATE_DISCONNECTED 1
-#define STATE_CONNECTED 2
-#define STATE_REQUEST_FAILED 3
-#define STATE_REQUESTED 4
-#define STATE_TIMED_OUT 5
-#define STATE_RESPONDED_NG 6
-#define STATE_RESPONDED_OK 7
-#define STATE_DISCONNECTED_ERROR 8
-
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Now: ");
+  
+  lcd.setCursor(0, 1);
+  lcd.print("Avg: ");
+    
   startTime = millis();
-  responseBuffer[0] = '\0';
-  state = STATE_DISCONNECTED;
-  retries = 0;
-
   readingSum = 0;
   numReadings = 0;
-
-  p("Initialized.");
 }
 
 void loop() {
   int flexiForceReading = analogRead(A0);
   readingSum += flexiForceReading;
   numReadings++;
-
-  switch (state) {
-    case STATE_DISCONNECTED:
-    case STATE_DISCONNECTED_ERROR:
-      if (ready()) {
-       if (connect()) {
-          p();
-          p("STATE_CONNECTED");
-          state = STATE_CONNECTED;
-        }
-
-        startTime = millis();
-      }
-      break;
-
-    case STATE_CONNECTED:
-      if (ready()) {
-        if (request(floor((float) readingSum / numReadings))) {
-          readingSum = 0;
-          numReadings = 0;
-
-          p();
-          p("STATE_REQUESTED");
-          state = STATE_REQUESTED;
-        }
-        else if (++retries < MAX_RETRIES) {
-          p();
-          p("STATE_REQUEST_FAILED");
-          p(String(MAX_RETRIES - retries) + " retries remaining.");
-          state = STATE_REQUEST_FAILED;
-        }
-        else {
-          p();
-          p("STATE_DISCONNECTED (1)");
-          state = STATE_DISCONNECTED_ERROR;
-          retries = 0;
-          client.stop();
-        }
-
-        startTime = millis();
-      }
-      break;
-
-    case STATE_REQUEST_FAILED:
-      if (ready()) {
-        p();
-        p("STATE_CONNECTED");
-        state = STATE_CONNECTED;
-
-        startTime = millis();
-      }
-      break;
-
-    case STATE_REQUESTED:
-      if (client.available()) {
-        if (receive()) {
-          p();
-          p("STATE_RESPONDED_OK");
-          state = STATE_RESPONDED_OK;
-          retries = 0;
-          client.stop();
-        }
-        else if (++retries < MAX_RETRIES) {
-          p();
-          p("STATE_RESPONDED_NG");
-          p(String(MAX_RETRIES - retries) + " retries remaining.");
-          state = STATE_RESPONDED_NG;
-        }
-        else {
-          p();
-          p("STATE_DISCONNECTED (1)");
-          state = STATE_DISCONNECTED_ERROR;
-          retries = 0;
-          client.stop();
-        }
-
-        startTime = millis();
-      }
-      else if (ready()) {
-        if (++retries < MAX_RETRIES) {
-          p();
-          p("STATE_TIMED_OUT");
-          p(String(MAX_RETRIES - retries) + " retries remaining.");
-          state = STATE_TIMED_OUT;
-        }
-        else {
-          p();
-          p("STATE_DISCONNECTED (2)");
-          state = STATE_DISCONNECTED_ERROR;
-          retries = 0;
-          client.stop();
-        }
-
-        startTime = millis();
-      }
-
-      break;
-
-    case STATE_TIMED_OUT:
-      if (ready()) {
-        p();
-        p("STATE_CONNECTED");
-        state = STATE_CONNECTED;
-
-        startTime = millis();
-      }
-      break;
-
-    case STATE_RESPONDED_NG:
-      if (ready()) {
-        p();
-        p("STATE_CONNECTED");
-        state = STATE_CONNECTED;
-
-        startTime = millis();
-      }
-      break;
-
-    case STATE_RESPONDED_OK:
-      if (ready()) {
-        p();
-        p("STATE_DISCONNECTED Ready to reconnect.");
-        state = STATE_DISCONNECTED;
-
-        startTime = millis();
-      }
-      break;
-
-    default:
-      p("Error: unknown state.");
-      break;
+  
+  unsigned long now = millis();
+  if (now - startTime > 1000) {
+    lcd.setCursor(5, 0);
+    lcd.print("           ");
+    lcd.setCursor(5, 0);
+    lcd.print(flexiForceReading);
+    
+    lcd.setCursor(5, 1);
+    lcd.print("           ");
+    lcd.setCursor(5, 1);
+    lcd.print(round((float) readingSum / numReadings));
+    
+    startTime = now;
+    readingSum = 0;
+    numReadings = 0;
   }
-
-  delay(250);
-}
-
-bool ready() {
-  unsigned long waitDuration;
-  switch (state) {
-    case STATE_DISCONNECTED_ERROR:
-      // Delay before attempting to connect after three retries failed.
-      waitDuration = 240000;
-      break;
-
-    case STATE_DISCONNECTED:
-      // Delay before attempting to connect.
-      waitDuration = 5000;
-      break;
-
-    case STATE_CONNECTED:
-      // Delay before making a request.
-      waitDuration = 5000;
-      break;
-
-    case STATE_REQUEST_FAILED:
-      // Delay before retry.
-      waitDuration = 30000;
-      break;
-
-    case STATE_REQUESTED:
-      // Delay before giving up on a response.
-      waitDuration = 30000;
-      break;
-
-    case STATE_TIMED_OUT:
-      // Delay before retry.
-      waitDuration = 30000;
-      break;
-
-    case STATE_RESPONDED_NG:
-      // Delay before retry.
-      waitDuration = 30000;
-      break;
-
-    case STATE_RESPONDED_OK:
-      // Delay before moving back to disconnected state.
-      waitDuration = 240000;
-      break;
-  }
-
-  if (millis() > startTime + waitDuration) {
-    return true;
-  }
-  else {
-    Serial.print(".");
-    return false;
-  }
-}
-
-bool connect() {
-  p();
-  p("Wifly trying to join network.");
-
-  WiFly.begin();
-  if (WiFly.join(ssid, passphrase)) {
-    p("Joined network.");
-    return true;
-  }
-  else {
-    p("Failed to join network. Waiting to try again.");
-    return false;
-  }
-}
-
-bool request(int reading) {
-  if (client.connected() || client.connect()) {
-    p();
-    p("Sending request.");
-    p(reading);
-    p();
-
-    client.flush();
-
-    // Arduino's apparently sketchy String class used only to convert int to char[].
-    String str;
-
-    char readingStr[5];
-    str = String(reading);
-    str.toCharArray(readingStr, 5);
-
-    clientPrintln(requestHttpCommand);
-    clientPrint(requestApiKey);
-    clientPrintln(xivelyApiKey);
-    clientPrintln(requestUserAgent);
-    clientPrintln(requestHost);
-    clientPrintln(requestContentType);
-    clientPrintln(requestContentLength);
-    clientPrintln(requestConnection);
-    clientPrintln();
-    clientPrint(requestDataPrefix);
-    clientPrint(readingStr);
-    clientPrint(requestDataSuffix);
-
-    // Pad the request with whitespace to meet the content length.
-    if (strlen(readingStr) < 4) clientPrint(" ");
-    if (strlen(readingStr) < 3) clientPrint(" ");
-    if (strlen(readingStr) < 2) clientPrint(" ");
-
-    clientPrintln();
-    clientPrintln();
-    clientPrintln();
-
-    p("Request sent.");
-    return true;
-  }
-  else {
-    p("Error connecting with remote host");
-    return false;
-  }
-}
-
-bool receive() {
-  char c;
-  int i;
-  bool gotHttp200 = false;
-
-  p();
-  p("--- BEGIN Receive Chunk");
-  while (client.available()) {
-    char c = client.read();
-    if (c == '\n' || c == '\r') {
-      if (responseBuffer[0] != '\0') {
-        p(responseBuffer);
-        gotHttp200 = gotHttp200 || strcmp(responseBuffer, http200);
-        responseBuffer[0] = '\0';
-      }
-    }
-    else if (strlen(responseBuffer) < RESPONSE_BUFFER_SIZE) {
-      i = strlen(responseBuffer);
-      responseBuffer[i] = c;
-      responseBuffer[i + 1] = '\0';
-    }
-  }
-  p("--- END Receive Chunk");
-
-  if (gotHttp200) {
-    p("Got HTTP 200!");
-  }
-
-  client.flush();
-
-  responseBuffer[0] = '\0';
-  return gotHttp200;
-}
-
-void p(String str) {
-  Serial.println(str);
-}
-
-void p(char* str) {
-  Serial.println(str);
-}
-
-void p(int str) {
-  Serial.println(str);
-}
-
-void p() {
-  Serial.println();
-}
-
-void clientPrint(char* str) {
-  Serial.print(str);
-  client.print(str);
-}
-
-void clientPrintln(char* str) {
-  Serial.println(str);
-  client.println(str);
-}
-
-void clientPrint(int x) {
-  client.print(x);
-  Serial.print(x);
-}
-
-void clientPrintln(int x) {
-  client.println(x);
-  Serial.println(x);
-}
-
-void clientPrintln() {
-  Serial.println();
-  client.println();
 }
