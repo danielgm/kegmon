0ffe3d80d40f7e3654d0a907029a5435d1860fb9
Niel McLaren <dan@danielmclaren.com>
2015-05-21 20:47:59 -0700

    Swap immediate reading for 'brewfulness' label.

--- kegmon/kegmon.ino
+++ kegmon/kegmon.ino
@@ -10,47 +10,47 @@
  * LCD D6 pin to digital pin 11
  * LCD D7 pin to digital pin 12
  */
-
 Adafruit_CharacterOLED lcd(OLED_V2, 6, 7, 8, 9, 10, 11, 12);
 
-unsigned long startTime;
+unsigned long readingStartTime;
 unsigned long readingSum;
 int numReadings;
+int prevValue;
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
-  
+
   lcd.setCursor(0, 0);
-  lcd.print("Now: ");
-  
-  lcd.setCursor(0, 1);
-  lcd.print("Avg: ");
-    
-  startTime = millis();
+  lcd.print("Brewfulness");
+
+  readingStartTime = millis();
   readingSum = 0;
   numReadings = 0;
+  prevValue = -1;
 }
 
 void loop() {
   int flexiForceReading = analogRead(A0);
   readingSum += flexiForceReading;
   numReadings++;
-  
+
   unsigned long now = millis();
-  if (now - startTime > 1000) {
-    lcd.setCursor(5, 0);
-    lcd.print("           ");
-    lcd.setCursor(5, 0);
-    lcd.print(flexiForceReading);
-    
-    lcd.setCursor(5, 1);
-    lcd.print("           ");
-    lcd.setCursor(5, 1);
-    lcd.print(round((float) readingSum / numReadings));
-    
-    startTime = now;
+  if (now - readingStartTime > 5000) {
+    int value = round((float) readingSum / numReadings);
+    if (value != prevValue) {
+      lcd.setCursor(0, 1);
+      lcd.print("    ");
+      lcd.setCursor(0, 1);
+      lcd.print(value);
+
+      prevValue = value;
+    }
+
+    readingStartTime = now;
     readingSum = 0;
     numReadings = 0;
   }
+
+  delay(100);
 }
